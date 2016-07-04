86affa8a125ad3fa28415e7a1ac9ae0dc82f5f41
Bjorn R. Jensen <bjensen@fastmail.fm>
2016-07-04 20:30:47 +0200

    added a test for retrieving a nonexiting object from memcache

--- tests/lib/Store/MemCacheTicketStoreTest.php
+++ tests/lib/Store/MemCacheTicketStoreTest.php
@@ -4,6 +4,31 @@ namespace SimpleSAML\Oauth2Server;
 
 class sspmod_oauth2server_Store_MemCacheTicketStoreTest extends \PHPUnit_Framework_TestCase
 {
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+
+        $configDirectory =  (dirname(__DIR__) . '/../../vendor/simplesamlphp/simplesamlphp/config/');
+
+        $file = fopen($configDirectory . 'config.php', 'w');
+
+        fwrite($file, sprintf('<?php
+/*
+ * The configuration of SimpleSAMLphp
+ *
+ */
+
+$config = array(
+    \'memcache_store.servers\' => array(
+        array(
+            array(\'hostname\' => \'localhost\'),
+        ),
+    ),
+);
+'));
+
+        fclose($file);
+    }
 
     public function testConstructor()
     {
@@ -15,5 +40,14 @@ class sspmod_oauth2server_Store_MemCacheTicketStoreTest extends \PHPUnit_Framewo
         $store = new \sspmod_oauth2server_Store_MemCacheTicketStore(array('prefix' => 'dummy'));
 
         $store->removeExpiredObjects();
+    }
+
+    public function testGetNonExistingObject()
+    {
+        $store = new \sspmod_oauth2server_Store_MemCacheTicketStore(array('prefix' => 'dummy'));
+
+        $object = $store->getObject('test');
+
+        $this->assertNull($object);
     }
 }
