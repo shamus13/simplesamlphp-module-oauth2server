d9518eed45705c5db404702e938d15f1faae0c21
Bjorn R. Jensen <bjensen@fastmail.fm>
2016-07-12 19:33:14 +0200

    use grep to extract the correct line instead of sed

--- tools/script/ldap_root.sh
+++ tools/script/ldap_root.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ldapsearch -Y EXTERNAL -H ldapi:/// -b "cn=config" "(olcSuffix=*)" olcSuffix|sed 's/.*olcSuffix: //g' | sed 's/ # search.*//g'
+ldapsearch -Y EXTERNAL -H ldapi:/// -b "cn=config" "(olcSuffix=*)" olcSuffix|grep -e olcSuffix: | sed 's/olcSuffix: //g'
