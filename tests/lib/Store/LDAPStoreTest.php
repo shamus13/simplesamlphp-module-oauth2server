e10c9f61e7b59bf0af38484ab8e19ae2a22cda92
Bjorn R. Jensen <bjensen@fastmail.fm>
2016-07-16 22:36:18 +0200

    added missing lgpl comment to unit tests

--- tests/lib/Store/LDAPStoreTest.php
+++ tests/lib/Store/LDAPStoreTest.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+*    simpleSAMLphp-oauth2server is an OAuth 2.0 authorization and resource server in the form of a simpleSAMLphp module
+*
+*    Copyright (C) 2014  Bjorn R. Jensen
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*
+*/
+
 namespace SimpleSAML\Oauth2Server;
 
 class sspmod_oauth2server_Store_LDAPStoreTest extends \PHPUnit_Framework_TestCase
--- tests/lib/Store/PostgresStoreTest.php
+++ tests/lib/Store/PostgresStoreTest.php
@@ -1,4 +1,24 @@
 <?php
+/*
+*    simpleSAMLphp-oauth2server is an OAuth 2.0 authorization and resource server in the form of a simpleSAMLphp module
+*
+*    Copyright (C) 2014  Bjorn R. Jensen
+*
+*    This library is free software; you can redistribute it and/or
+*    modify it under the terms of the GNU Lesser General Public
+*    License as published by the Free Software Foundation; either
+*    version 2.1 of the License, or (at your option) any later version.
+*
+*    This library is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*    Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public
+*    License along with this library; if not, write to the Free Software
+*    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*
+*/
 
 namespace SimpleSAML\Oauth2Server;
 
@@ -174,11 +194,13 @@ $config = array(
         $this->assertNull($object4);
     }
 
-    public function getId() {
+    public function getId()
+    {
         return \SimpleSAML\Utils\Random::generateID();
     }
 
-    public function evilObjectCreator($id) {
+    public function evilObjectCreator($id)
+    {
         $dsn = $this->config['dsn'];
         $username = $this->config['username'];
         $password = $this->config['password'];
